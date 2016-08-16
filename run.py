dc0ba9f509ecbc094f05b6073253766ce7a3b289
Josh Zheng <denial@gmail.com>
2016-08-16 02:34:54 -0700

    find by ingredients working.

---
+++ run.py
@@ -0,0 +1,29 @@
+import json
+import os
+import time
+from dotenv import load_dotenv
+from slackclient import SlackClient
+from watson_developer_cloud import ConversationV1
+
+from recipe import RecipeClient
+from souschef import SousChef
+
+if __name__ == "__main__":
+  load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+
+  bot_id = os.environ.get("BOT_ID")
+
+  slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+  conversation_client = ConversationV1(
+    username=os.environ.get("CONVERSATION_USERNAME"),
+    password=os.environ.get("CONVERSATION_PASSWORD"),
+    version='2016-07-11')
+
+  recipe_client = RecipeClient(os.environ.get("SPOONACULAR_KEY"))
+
+  souschef = SousChef(bot_id, 
+                      slack_client, 
+                      conversation_client,
+                      recipe_client)
+  souschef.run()
---
+++ souschef.py
@@ -0,0 +1,136 @@
+import json
+import os
+import time
+from slackclient import SlackClient
+from recipe import RecipeClient
+from pprint import pprint
+
+class SousChef:
+  def __init__(self, bot_id, 
+               slack_client, conversation_client, recipe_client):
+    self.bot_id = bot_id
+
+    self.slack_client = slack_client
+    self.conversation_client = conversation_client
+    self.recipe_client = recipe_client
+    
+    self.at_bot = "<@" + bot_id + ">:"
+    self.delay = 0.5 #second
+    self.workspace_id = '93054028-2b0b-4b21-9bc1-ec8c48970dbb'
+
+    self.context = {
+      "user": "there"
+    }
+  
+  def parse_slack_output(self, slack_rtm_output):
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+      for output in output_list:
+        if output and 'text' in output and \
+          'user_profile' not in output:
+          return output['text'].strip().lower(), \
+                 output['channel']
+    return None, None
+
+  def post_to_slack(self, response, channel):   
+    self.slack_client.api_call("chat.postMessage", 
+                          channel=channel,
+                          text=response, as_user=True)
+
+  def make_formatted_steps(self, recipe_info, recipe_steps):
+    response = "Ok, it takes *" +\
+      str(recipe_info['readyInMinutes']) +\
+      "* minutes to make *" +\
+      str(recipe_info['servings']) +\
+      "* servings of *" +\
+      recipe_info['title'] + "*. Here are the steps:\n\n"
+
+    for i, r_step in enumerate(recipe_steps[0]['steps']):
+      equip_str = ""
+      for e in r_step['equipment']:
+        equip_str += e['name'] + ", "
+      if not equip_str:
+        equip_str = "None"
+      else:
+        equip_str = equip_str[:-1]
+
+      response += "*Step " + str(i+1) + "*:\n" +\
+        "_Equipment_: " + equip_str + "\n" +\
+        "_Action_: " + r_step['step'] + "\n\n"
+
+    response += "Say anything to start over..."
+    return response
+
+  def handle_ingredients_message(self, message):
+    if self.context['get_ingredients']:
+      self.context['recipes'] = \
+        self.recipe_client.find_by_ingredients(message)
+      
+    response = "Lets see here...\n" + \
+               "I've found these recipes: \n"
+    #import pdb;pdb.set_trace()
+    for i, recipe in enumerate(self.context['recipes']):
+      response += str(i+1) + ". " + recipe['title'] + "\n"
+    response += "\nPlease enter the corresponding number of your choice."
+
+    return response
+
+  def handle_selection_message(self, selection):
+    recipe_id = self.context['recipes'][selection-1]['id']
+    recipe_info = self.recipe_client.get_info_by_id(recipe_id)
+    recipe_steps = self.recipe_client.get_steps_by_id(recipe_id)
+
+    return self.make_formatted_steps(recipe_info, recipe_steps)
+
+  def handle_message(self, message, channel):
+    watson_response = self.conversation_client.message(
+      workspace_id=self.workspace_id, 
+      message_input={'text': message},
+      context=self.context)
+
+    self.context = watson_response['context']
+
+    print
+    pprint(watson_response)
+    print
+
+    if 'is_ingredients' in self.context.keys() and \
+        self.context['is_ingredients']:
+      response = self.handle_ingredients_message(message)
+
+    elif 'is_selection' in self.context.keys() and \
+       self.context['is_selection']:
+      selection = int(self.context['selection'])
+
+      if selection >= 1 and selection <= 5:
+        self.context['selection_valid'] = True
+        response = self.handle_selection_message(selection)
+      else:
+        self.context['selection_valid'] = False
+
+        print 
+        print "RECIPES"
+        pprint(self.context['recipes'])
+        print "RECIPES"
+        print
+        response = "Invalid selection! " +\
+          "Say anything to see your choices again..."
+
+    else:
+      response = ''
+      for text in watson_response['output']['text']:
+        response += text + "\n"
+
+    self.post_to_slack(response, channel)
+
+  def run(self):
+    if self.slack_client.rtm_connect():
+      print("sous-chef is connected and running!")
+      while True:
+        slack_output = self.slack_client.rtm_read()
+        message, channel = self.parse_slack_output(slack_output)
+        if message and channel:
+          self.handle_message(message, channel)
+        time.sleep(self.delay)
+    else:
+      print("Connection failed. Invalid Slack token or bot ID?")
